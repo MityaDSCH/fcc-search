f66c98d93e296045131a1ee43d0a8d5385f7abdc
mlsgearon <mlgearon@gmail.com>
2015-12-31 18:22:52 -0600

    adds search feature

--- src/history.model.js
+++ src/history.model.js
@@ -4,7 +4,7 @@ var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 
 var History = new Schema({
-
+  array: Object
 });
 
 module.exports = mongoose.model('History', History);
--- src/main.js
+++ src/main.js
@@ -1,5 +1,7 @@
 'use strict';
 
+var url = require('url');
+
 var express = require('express');
 var mongoose = require('mongoose');
 require('dotenv').load();
@@ -8,6 +10,14 @@ var Bing = require('node-bing-api')({ accKey: process.env.BING_KEY });
 
 var mongoURI = process.env.MONGOLAB_URI || 'mongodb://localhost:27017/fcc-search';
 mongoose.connect(mongoURI);
+
+var History = require('./history.model.js');
+
+History.find({}).remove().exec(function() {
+  History.create({
+    array: []
+  });
+});
 
 var app = express();
 
@@ -18,12 +28,33 @@ var app = express();
 app.route('/api/imagesearch/:query')
   .get(function(req, res) {
     // parse out search term and offset
+    var data = url.parse(req.url.substr(req.url.lastIndexOf('/') + 1), true, true);
+    var search = decodeURI(data.pathname);
+    var offset = data.query.offset;
     // save search term and time to db
-    // search flickrapi and return
-      // url
-      // snippet
-      // thumbnail
-      // context
+    History.findOneAndUpdate(
+      {},
+      {$push: {array: {
+        term: search,
+        when: new Date().toISOString()
+      }}},
+      function(err, model) {
+        console.log(err, model);
+      }
+    );
+    // search api and return
+    Bing.images(search, {skip: offset || 0}, function(err, bingRes, body) {
+      var reducedInfo = body.d.results.map(function(image) {
+        return {
+          url: image.MediaUrl,
+          snippet: image.Title,
+          thumbnail: image.Thumbnail.MediaUrl,
+          context: image.SourceUrl
+        }
+      });
+      res.json(reducedInfo);
+      res.end();
+    });
   });
 
 app.route('/api/latest/imagesearch')
